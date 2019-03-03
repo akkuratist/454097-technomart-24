767c87483f06d5282d76e2f4ffe0b0f7c508322e
akkuratist <36897138+akkuratist@users.noreply.github.com>
2019-03-03 16:51:28 +0300

    Добавил часть скриптов

---
+++ js/script.js
@@ -0,0 +1,122 @@
+
+// Форма обратной связи
+
+var writeUs = document.querySelector(".btn-write-us");
+var popup = document.querySelector(".write-us");
+var close = document.querySelector(".modal-close");
+
+var form = popup.querySelector("form");
+var username = popup.querySelector("[name=username]");
+var email = popup.querySelector("[type=email]");
+var letter = popup.querySelector("textarea");
+
+writeUs.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("show-me");
+  username.focus();
+})
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("show-me");
+  popup.classList.remove("modal-error");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("show-me") ) {
+      popup.classList.remove("show-me");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
+
+form.addEventListener("submit", function (evt){
+  if (!username.value || !email.value || !letter.value) {
+  evt.preventDefault();
+  popup.classList.remove("modal-error");
+  popup.offsetWidth = popup.offsetWidth;
+  popup.classList.add("modal-error");
+  }
+});
+
+// Карта
+
+var mapPreview = document.querySelector(".show-map");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapPreview.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("show-me");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  mapPopup.classList.remove("show-me");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (mapPopup.classList.contains("show-me")) {
+      mapPopup.classList.remove("show-me");
+      mapPopup.classList.remove("modal-error");
+    }
+  }
+});
+
+// Слайдер сервисы
+
+var buttonDelivery = document.querySelector(".btn-delivery");
+var serviceDelivery = document.querySelector(".service-item-delivery");
+var buttonWarranty = document.querySelector(".btn-warranty");
+var serviceWarranty = document.querySelector(".service-item-warranty");
+var buttonCredit = document.querySelector(".btn-credit");
+var serviceCredit = document.querySelector(".service-item-credit");
+
+buttonDelivery.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  buttonDelivery.classList.add("btn-services-current");
+  serviceDelivery.classList.add("service-item-current");
+  buttonWarranty.classList.remove("btn-services-current")
+  serviceWarranty.classList.remove("service-item-current");
+  buttonCredit.classList.remove("btn-services-current");
+  serviceCredit.classList.remove("service-item-current");
+});
+
+buttonWarranty.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  buttonWarranty.classList.add("btn-services-current");
+  serviceWarranty.classList.add("service-item-current");
+  buttonDelivery.classList.remove("btn-services-current");
+  serviceDelivery.classList.remove("service-item-current");
+  buttonCredit.classList.remove("btn-services-current");
+  serviceCredit.classList.remove("service-item-current");
+})
+
+buttonCredit.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  buttonCredit.classList.add("btn-services-current");
+  serviceCredit.classList.add("service-item-current");
+  buttonDelivery.classList.remove("btn-services-current");
+  serviceDelivery.classList.remove("service-item-current");
+  buttonWarranty.classList.remove("btn-services-current")
+  serviceWarranty.classList.remove("service-item-current");
+});
+
+
+// Корзина
+
+let buttonBuy = document.querySelectorAll(".btn-buy");
+let cartPopup = document.querySelector(".modal-cart");
+let cartClose = document.querySelector(".modal-close");
+
+buttonBuy.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  cartPopup.classList.add("cart-show-me");
+} );
+
+cartClose.addEventListener("click", function (evt) {
+  cartPopup.classList.remove("cart-show-me");
+});
